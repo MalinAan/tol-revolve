79ea5462b00a1f15d22c88c214b440aa1e24421b
Kyrre <kyrrehg@ifi.uio.no>
2017-06-07 19:38:06 +0200

    first try at evolving for obstacles

--- tools/models/obstacles/world-generator.py
+++ tools/models/obstacles/world-generator.py
@@ -7,29 +7,32 @@ from sdfbuilder.structure import Box, Cylinder, Collision, Visual, StructureComb
 import math
 
 sdf = SDF()
-model = Model("obstacles")
+model = Model("obstacles",static=True)
 
-for y in range(-5,5):
-    for x in range(-5,5):
-        l = Link("box")
 
-        #box_geom = Box(1.0, 1.0, 1.0, mass=0.5)
-        #b = StructureCombination("box", box_geom)
-        #l.add_element(b)
+def gen_boxes(dimensions=4, spacing=0.5, size=0.05):
+        for x in range(-dimensions,dimensions+1):
+            for y in range(-dimensions,dimensions+1):
+                l = Link("box")
 
-        l.make_box(1, 0.1, 0.1, 0.01+0.01*abs(x))
+                #box_geom = Box(1.0, 1.0, 1.0, mass=0.5)
+                #b = StructureCombination("box", box_geom)
+                #l.add_element(b)
 
-        pos = Vector3(0.6*x,0.6*y,0)
+                if (x!=0 or y!=0):
+                    l.make_box(1, size, size, 0.01*max(abs(x),abs(y)))
 
-        l.set_position(pos)
-        l.rotate_around(Vector3(0, 0, 1), math.radians(x*4), relative_to_child=False)
+                pos = Vector3(spacing*x,spacing*y,0)
 
-        model.add_element(l)
+                l.set_position(pos)
+                l.rotate_around(Vector3(0, 0, 1), math.radians(x*y), relative_to_child=False)
+
+                model.add_element(l)
 
 #adjust it up to ground plane
 model.set_position(Vector3(0, 0, 0.0))
 
 sdf.add_element(model)
 
-
+gen_boxes(dimensions=4,spacing=0.15, size=0.03)
 print(str(sdf))
