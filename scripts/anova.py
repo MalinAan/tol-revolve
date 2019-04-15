d16327be992cff2e55154ae678c8ebd72087ea10
Malin Aandahl <malinaandahl@gmail.com>
2019-04-15 12:34:46 +0200

    Cleaning

--- scripts/anova.py
+++ scripts/anova.py
@@ -39,49 +39,57 @@ def stars(p):
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
 
-data_lists = {}
-for folder in get_immediate_subdirectories(sys.argv[1]):
-    #print folder
-    data_lists[folder]=[]
-    csv_files = ["generations.csv","robots.csv"] #the rest is hardcoded for these two files for now
-    for datafile in csv_files:
-        fn = os.path.join(sys.argv[1],folder,datafile)
-        # print fn
-        data_lists[folder].append(pd.read_csv(fn, index_col=False))
+
+def get_data_list(exp_name):
+    data_lists = {}
+    for folder in get_immediate_subdirectories(exp_name):
+        #print folder
+        data_lists[folder]=[]
+        csv_files = ["generations.csv","robots.csv"] #the rest is hardcoded for these two files for now
+        for datafile in csv_files:
+            fn = os.path.join(exp_name,folder,datafile)
+            # print fn
+            data_lists[folder].append(pd.read_csv(fn, index_col=False))
+
+    return data_lists
 #print list(data_lists)
+
+def get_last_generation(data_lists, fitness_function):
+    last_gens = {}
+    first_gens = {}
+    #print("keys", data_lists.keys())
+    #print("0", data_lists["world0_old_fitness"][0])
+    #print("0", data_lists["world0_old_fitness"][1])
+    for exp in data_lists.keys():
+        #print exp
+        #merging the two data frames. Assuming only two now
+        #not perfect, should not duplicate the run columns
+        merged = pd.merge(data_lists[exp][0], data_lists[exp][1], left_on=['robot_id'], right_on=['id'])
+        
+
+        merged["fitness_function"] = fitness_function
+        merged["world"] = exp[0:6]
+        if(exp == "world0_old_fitness"):
+            print ("LIST", list(merged))
+            print("MERGED", merged)
+        #find and extract last and first generations
+        max_gen = merged['gen'].idxmax()
+        last_gen_idx=merged['gen'][max_gen]
+        #print "last gen:", last_gen_idx
+        last_gens[exp]=merged[merged['gen']==last_gen_idx]
+        first_gens[exp]=merged[merged['gen']==0]
+    return last_gens
+
+
+data_lists = get_data_list(sys.argv[1])
+last_gens = get_last_generation(data_lists, sys.argv[1])
         
 
 ##############################################
 #merge and extract first and last generations
 ##############################################
-last_gens = {}
-first_gens = {}
-print("keys", data_lists.keys())
-print("0", data_lists["world0_old_fitness"][0])
-print("0", data_lists["world0_old_fitness"][1])
-for exp in data_lists.keys():
-    #print exp
-    #merging the two data frames. Assuming only two now
-    #not perfect, should not duplicate the run columns
-    merged = pd.merge(data_lists[exp][0], data_lists[exp][1], left_on=['robot_id'], right_on=['id'])
-    
-    print("world cut ", exp[0:6])
-    merged["world"] = exp[0:6]
-    if(exp == "world0_old_fitness"):
-        print ("LIST", list(merged))
-        print("MERGED", merged)
-    #find and extract last and first generations
-    max_gen = merged['gen'].idxmax()
-    last_gen_idx=merged['gen'][max_gen]
-    #print "last gen:", last_gen_idx
-    last_gens[exp]=merged[merged['gen']==last_gen_idx]
-    first_gens[exp]=merged[merged['gen']==0]
 
-    #checking the data
-    #print "run count: ", merged['run_x'].nunique()
-    #print "number of first generations extracted: ", last_gens[exp]['run_x'].nunique()
-    #print "number of last generations extracted: ", last_gens[exp]['run_x'].nunique()
-    #print "number of solutions over all runs: ",  len(last_gens[exp])
+
 
 def print_stats(data, exp, measure):
     print("Experiment: %s", exp)
@@ -100,10 +108,13 @@ def print_stats(data, exp, measure):
 #print(last_gens)
 #print(last_gens.to_string())
 comb = pd.concat(last_gens)
+print(comb.describe().to_string())
+comb = comb.drop(columns=['run_y', 'id'])
+print(comb.describe().to_string())
 
 #print(comb["robot_id"])
 #print(comb.to_string())
-#print(comb["run_x"])
+print(comb["world"])
 #print("UNNAMED", comb["unnamed"])
 #print(vars(comb))
 
